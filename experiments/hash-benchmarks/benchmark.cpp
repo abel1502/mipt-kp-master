ec2c3373473ff4f51302b1ccd7bbda6d98acf844
abel1502 <terragame.abel@gmail.com>
2025-04-14 23:44:19 +0300

    Add benchmark.cpp

---
+++ experiments/hash-benchmarks/benchmark.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <utility>
+
+int main(int argc, const char **argv) {
+    printf("Not implemented yet\n");
+    
+    return 0;
+}
