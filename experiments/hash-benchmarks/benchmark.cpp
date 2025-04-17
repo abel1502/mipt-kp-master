28b8cda0ea1a81e02171685ff0aba05ccb526245
abel1502 <terragame.abel@gmail.com>
2025-04-17 15:05:53 +0300

    Add benchmark selection

--- experiments/hash-benchmarks/benchmark.cpp
+++ experiments/hash-benchmarks/benchmark.cpp
@@ -2,9 +2,57 @@
 #include <cstdlib>
 #include <memory>
 #include <utility>
+#include <string_view>
 
 int main(int argc, const char **argv) {
-    printf("Not implemented yet\n");
-    
-    return 0;
+    if (argc != 2) {
+        printf(
+            "Usage: %s <benchmark>\n"
+            "\n"
+            "Available benchmarks:\n"
+            "  rabin\n"
+            "  adler32\n"
+            "  cyclic\n"
+            "  md5\n"
+            "  sha256\n"
+            "  highway\n",
+            argv[0]
+        );
+        return -1;
+    }
+
+    std::string_view benchmark = argv[1];
+
+    if (benchmark == "rabin") {
+        // TODO
+        return 0;
+    }
+
+    if (benchmark == "adler32") {
+        // TODO
+        return 0;
+    }
+
+    if (benchmark == "cyclic") {
+        // TODO
+        return 0;
+    }
+
+    if (benchmark == "md5") {
+        // TODO
+        return 0;
+    }
+
+    if (benchmark == "sha256") {
+        // TODO
+        return 0;
+    }
+
+    if (benchmark == "highway") {
+        // TODO
+        return 0;
+    }
+
+    printf("Unknown benchmark: %s\n", argv[1]);
+    return -1;
 }
